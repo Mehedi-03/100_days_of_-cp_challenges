ee4a1ff6b23d1440469277f7d44db72ad5525a7b
Mehedi-hasan <mehedi24953@gmail.com>
2025-09-06 10:44:48 +0600

    6 day problem solve

---
+++ A_Digits_Sum.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t; 
+    cin>>t;
+    while(t--)
+    {
+        long long n; 
+        cin>>n; 
+        int result = int(n + 1) / 10; 
+        cout<<result<<endl; 
+    }
+    return 0; 
+}
