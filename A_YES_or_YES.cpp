bc9f886dc9706b95ca1c222995abdea506564602
Mehedi-hasan <mehedi24953@gmail.com>
2025-09-18 20:36:58 +0600

    Day 18 problem solve

---
+++ A_YES_or_YES.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t; 
+    cin>>t; 
+    while(t--)
+    {
+        string s; 
+        cin>>s; 
+        if(s == "YES" || s == "Yes" || s == "yES" || s == "yes" || s == "YeS" || s == "YEs" || s== "yEs" || s == "yeS")
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0; 
+}
