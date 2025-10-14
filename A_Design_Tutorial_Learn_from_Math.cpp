d5978858fbf95bc3c5da4ac9a9a9453c03d6130e
Mehedi-hasan <mehedi24953@gmail.com>
2025-10-14 23:04:15 +0600

    43 day problem solve

---
+++ A_Design_Tutorial_Learn_from_Math.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n; 
+    cin>>n; 
+    int x , y; 
+    if(n % 2 == 0)
+    {
+        x = 4; 
+        y = n - 4; 
+    }
+    else
+    {
+        x = 9; 
+        y = n - 9; 
+    }
+    cout<<x<<" "<<y<<endl;
+    return 0; 
+}
