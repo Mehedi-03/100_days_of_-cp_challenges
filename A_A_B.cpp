8152c3be4a9b0fe6b8b6846ebd831cd72e915e2b
Mehedi-hasan <mehedi24953@gmail.com>
2025-10-24 18:52:55 +0600

    53 day problem solve

---
+++ A_A_B.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t; 
+    cin>>t; 
+    while(t--)
+    {
+        int a, b;
+        char ch;
+        cin>>a>>ch>>b; 
+        int result = a + b; 
+        cout<<result<<endl; 
+    }
+    return 0; 
+}
