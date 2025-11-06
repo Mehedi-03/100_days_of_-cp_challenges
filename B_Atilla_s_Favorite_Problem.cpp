db0a5079269e5b3273b29c4fa9a8ebd1e4a7b1ca
Mehedi-hasan <mehedi24953@gmail.com>
2025-11-06 20:49:17 +0600

    65 day problem solve

---
+++ B_Atilla_s_Favorite_Problem.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t; 
+    cin>>t; 
+    while(t--)
+    {
+        int n; 
+        cin>>n; 
+        string s; 
+        cin>>s; 
+        sort(s.begin(), s.end()); 
+        int result = s.back() - 'a'; 
+        cout<<result + 1<<endl; 
+    
+    }
+    return 0; 
+}
