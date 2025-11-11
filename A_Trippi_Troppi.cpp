de755261b1354a37f25351e108d48a7b6e3affaf
Mehedi-hasan <mehedi24953@gmail.com>
2025-11-11 18:00:38 +0600

    61 problem solve

---
+++ A_Trippi_Troppi.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t; 
+    cin>>t; 
+    while (t--)
+    {
+        string a, b, c;
+        cin>>a>>b>>c;
+        cout<<a[0];
+        cout<<b[0]; 
+        cout<<c[0];
+        cout<<endl; 
+    }
+    
+    return 0; 
+}
