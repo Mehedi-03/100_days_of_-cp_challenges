9b8e4386b7badf9d0e1059e513a79a896ba1da83
Mehedi-hasan <mehedi24953@gmail.com>
2025-11-19 16:07:38 +0600

    some change

--- A_Square.cpp
+++ A_Square.cpp
@@ -1,22 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int t; 
-    cin>>t; 
-    while(t--)
-    {
-        int a, b, c, d; 
-        cin>>a>>b>>c>>d; 
-        if(a == b && a == c && a == d)
-        {
-            cout<<"YES"<<endl; 
-        }
-        else
-        {
-            cout<<"NO"<<endl; 
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int x[4], y[4];
+
+        for (int i = 0; i < 4; i++) {
+            cin >> x[i] >> y[i];
         }
 
+        int side = 0;
+
+    
+        for (int i = 0; i < 4; i++) {
+            for (int j = i + 1; j < 4; j++) {
+                if (x[i] == x[j]) {
+                    side = abs(y[i] - y[j]);  
+                } 
+                else if (y[i] == y[j]) {
+                    side = abs(x[i] - x[j]);   
+                }
+            }
+        }
+
+        cout << side * side << endl; 
     }
-    return 0; 
+
+    return 0;
 }
